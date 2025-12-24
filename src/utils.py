f2724e75213cf91dd5870dd50aa4c8f9e7e6a6cf
Cong Khai <dangcongkhai2k5@gmail.com>
2025-12-24 13:13:59 +0700

    feat: add error analysis across different facial forgery methods

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import random
 import numpy as np
 import torch.nn.functional as F
@@ -8,8 +9,17 @@ import seaborn as sns
 import json
 from torch.utils.data import Dataset, DataLoader
 from sklearn.metrics import f1_score, roc_auc_score
+from PIL import Image
+from .preprocessing import get_transformations
 from .train import get_all_predictions
-plt.style.use('ggplot')
+import os
+import re
+
+plt.style.use("ggplot")
+
+current_dir = os.getcwd()
+project_root = os.path.abspath(os.path.join(current_dir, ".."))
+
 
 # NOTE - Set seed
 def set_seed(seed=42):
@@ -19,27 +29,25 @@ def set_seed(seed=42):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    
+
 
 # NOTE. Save Checkpoint & Experiment
 def save_checkpoint(model, optimizer, filename="checkpoint.pth"):
-    checkpoint = {
-        'state_dict': model.state_dict(),
-        'optimizer': optimizer.state_dict()
-    }
+    checkpoint = {"state_dict": model.state_dict(), "optimizer": optimizer.state_dict()}
     torch.save(checkpoint, filename)
     print("Checkpoint saved:", filename)
-    
+
+
 def save_experiment_json(history, filename):
     """
     Saves the experiment history to a JSON file.
     """
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         json.dump(history, f, indent=4)
     print(f"History saved to {filename}")
- 
 
-# NOTE. Get device   
+
+# NOTE. Get device
 def get_device():
     if torch.cuda.is_available():
         return "cuda"
@@ -47,7 +55,7 @@ def get_device():
         return "mps"
     else:
         return "cpu"
-    
+
 
 # NOTE. Plot History
 def plot_history(history, title, save_path=None, start_finetuned_epoch=None):
@@ -111,27 +119,130 @@ def plot_history(history, title, save_path=None, start_finetuned_epoch=None):
     plt.tight_layout()
     plt.show()
 
+
 # NOTE. Performance Evaluation
-def performance(model, loader, device, model_name, save_path = None, class_names=['real', 'fake']):
+def performance(
+    model, loader, device, model_name, save_path=None, class_names=["real", "fake"]
+):
     from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+
     _, y_true, y_pred = get_all_predictions(model, loader, device)
-    
+
     acc = accuracy_score(y_true, y_pred)
     print(f"Accuracy score: {acc:.4f}")
-    
+
     print("Classification Report")
     print(classification_report(y_true, y_pred))
-    
+
     cm = confusion_matrix(y_true, y_pred)
     plt.figure(figsize=(8, 6))
     sns.heatmap(
-        cm, annot=True, fmt='d', cmap='crest',
+        cm,
+        annot=True,
+        fmt="d",
+        cmap="crest",
         xticklabels=class_names,
-        yticklabels=class_names)
-    plt.xlabel('Predicted Label')
-    plt.ylabel('True Label')
-    plt.title(f'{model_name} Confusion Matrix')
+        yticklabels=class_names,
+    )
+    plt.xlabel("Predicted Label")
+    plt.ylabel("True Label")
+    plt.title(f"{model_name} Confusion Matrix")
     if save_path is not None:
         plt.savefig(save_path)
     plt.tight_layout()
+    plt.show()
+
+
+def get_labels(logits):
+    return (
+        torch.argmax(nn.Softmax(dim=-1)(logits), dim=1)
+        .cpu()
+        .detach()
+        .flatten()
+        .tolist()
+    )
+
+
+# NOTE. Error analysis
+def error_analysis(
+    model,
+    fake_image_path,
+    model_name,
+    device,
+    batch_size=32,
+    use_fourier_transform=False,
+):
+    fake_methods = [
+        "Deepfakes",
+        "Face2Face",
+        "FaceSwap",
+        "FaceShifter",
+        "NeuralTextures",
+    ]
+    summary = {}
+    for method in fake_methods:
+        summary[method] = {"total": 0, "correct_pred": 0}
+    items = os.listdir(fake_image_path)
+    model.eval()
+    transform = get_transformations(model_name, use_augmentation=False)
+    pattern = r"(train|val|test)"
+    data_split = re.findall(pattern, fake_image_path)[0]
+    for i in range(0, len(items), batch_size):
+        items_batch = items[i : i + batch_size]
+        images_batch = []
+        fourier_mags_batch = []
+        methods = []
+        # Read each image and stack
+        for item in items_batch:
+            method = item.split("_")[2]  # Eg: fake_849_Deepfakes_051.png -> Deepfakes
+            methods.append(method)
+            summary[method]["total"] += 1
+            image_path = os.path.join(fake_image_path, item)
+            image_name = item.split(".")[0]
+
+            image = np.array(Image.open(image_path).convert("RGB"))
+            image = transform(image=image)["image"]
+            images_batch.append(image)
+
+            if use_fourier_transform:
+                # Get fourier transform
+                fourier_mag = torch.tensor(
+                    np.load(f"{project_root}/fourier/{data_split}/{image_name}.npy"),
+                    dtype=torch.float32,
+                ).unsqueeze(0)
+                fourier_mags_batch.append(fourier_mag)
+        batch_tensors = torch.stack(images_batch, dim=0).to(device=device)
+        
+        
+        with torch.inference_mode():
+            if use_fourier_transform:
+                batch_fourier_mags_tensor = torch.stack(fourier_mags_batch, dim=0).to(device=device)
+                logits = model(batch_fourier_mags_tensor, batch_tensors)
+            else:
+                logits = model(batch_tensors)
+            labels = get_labels(logits)
+            for label, method in zip(labels, methods):
+                if label == 1:
+                    summary[method]["correct_pred"] += 1
+    for method in fake_methods:
+        print(
+            f"{method} : {summary[method]['correct_pred'] / summary[method]['total']:.4f}"
+        )
+    return summary
+
+
+def plot_error_analysis(summary, model_name, save_path=None):
+    methods = list(summary.keys())
+    values = [round(summary[method]['correct_pred']/summary[method]['total'], 4) for method in methods]
+    colors = ['#FF9999', '#66B2FF', '#99FF99', '#FFCC99', '#CC66FF'] 
+    fig, ax = plt.subplots(figsize=(8, 6))
+    bars = ax.barh(methods, values, color=colors)
+    ax.bar_label(bars)
+    plt.xlabel('Facial Forgery Methods')
+    plt.ylabel('Accuracy')
+    plt.xlim(0, 1.1)
+    plt.title(f'Accuracy on different facial forgery methods for {model_name}')
+    plt.grid(False)
+    if save_path is not None:
+        plt.savefig(save_path)
     plt.show()
